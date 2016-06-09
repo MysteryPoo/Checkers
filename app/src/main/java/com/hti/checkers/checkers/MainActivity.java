512d03ee59dce18414f3a295740a271922bdb20d
MysteryPoo <Matt.Thornton@knights.ucf.edu>
2016-06-09 11:28:47 -0400

    Setup the board and add pieces
    The board is now setup at start with a bunch of pre-sized empty
    FrameLayouts. Then pieces can be added fairly easily in a gridlike logic
    with a color, x, and y value.
    
    Next step, make the pieces interactive.

--- app/src/main/java/com/hti/checkers/checkers/MainActivity.java
+++ app/src/main/java/com/hti/checkers/checkers/MainActivity.java
@@ -2,6 +2,11 @@ package com.hti.checkers.checkers;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.TypedValue;
+import android.view.Gravity;
+import android.widget.FrameLayout;
+import android.widget.GridLayout;
+import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +14,40 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setupBoard();
+
+        newPiece(R.drawable.black, 1, 0);
+        newPiece(R.drawable.red, 5, 3);
+    }
+
+    private void newPiece(int color, int x, int y) {
+        GridLayout gl = (GridLayout)findViewById(R.id.gridLayout);
+        FrameLayout fl = (FrameLayout)gl.getChildAt(getIndex(x, y));
+        ImageButton piece = new ImageButton(this);
+        piece.setBackgroundResource(color);
+        fl.addView(piece);
+    }
+
+    private int getIndex(int x, int y) {
+        return x * 8 + y;
+    }
+
+    private void setupBoard() {
+        GridLayout gl = (GridLayout)findViewById(R.id.gridLayout);
+        for(int x = 0; x < 8; ++x) {
+            for(int y = 0; y < 8; ++y) {
+                GridLayout.LayoutParams lp = new GridLayout.LayoutParams();
+                lp.width = (int)TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 52, getResources().getDisplayMetrics());
+                lp.height = (int)TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 54, getResources().getDisplayMetrics());
+                lp.setGravity(Gravity.FILL);
+                lp.columnSpec = GridLayout.spec(x);
+                lp.rowSpec = GridLayout.spec(y);
+
+                FrameLayout fl = new FrameLayout(this);
+                fl.setLayoutParams(lp);
+
+                gl.addView(fl);
+            }
+        }
     }
 }
